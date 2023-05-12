4d30efeed313bead91a8f1c86b5283fce864aec9
a18887 <a18887@alunos.ipca.pt>
2023-05-12 19:38:37 +0100

    Criação de base de dados e inserção de user

---
+++ app/src/main/java/com/example/gamestate/ui/data/User.kt
@@ -0,0 +1,14 @@
+package com.example.gamestate.ui.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User (
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val username: String,
+    val email: String,
+    val password: String,
+    //val country: String
+)
