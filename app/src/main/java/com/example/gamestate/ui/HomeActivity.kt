1491e8a3735206bdb29f61a29b99da3161c2d0eb
Alex18891 <a18891@alunos.ipca.pt>
2023-05-31 23:35:56 +0100

    Glide to homeactivity

--- app/src/main/java/com/example/gamestate/ui/HomeActivity.kt
+++ app/src/main/java/com/example/gamestate/ui/HomeActivity.kt
@@ -12,6 +12,7 @@ import android.widget.*
 import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.gamestate.R
 import com.example.gamestate.ui.data.Home.RecViewHomeAdapter
 import com.example.gamestate.ui.data.Home.SpinnerAdapter
@@ -39,9 +40,84 @@ class HomeActivity : AppCompatActivity() {
         val sharedPreferences = application.getSharedPreferences("login", Context.MODE_PRIVATE)
         val loginAutomatic = sharedPreferences.getString("username","")
         val searchgametext : EditText = findViewById(R.id.home_search_edit_text)
+        val firstimg : ImageView = findViewById(R.id.first_game)
+        val secondimg : ImageView = findViewById(R.id.second_game)
+        val thirdimg : ImageView = findViewById(R.id.thirdgame)
+        val fourthimg : ImageView = findViewById(R.id.fourth_game)
+        val fiftyimg : ImageView = findViewById(R.id.fifth_game)
+        val sixtyimg : ImageView = findViewById(R.id.sixth_game)
         val recyclerView = findViewById<RecyclerView>(R.id.home_games_recyclerview)
         username.setText(loginAutomatic)
 
+
+        fun imgpopular(){
+            val server_ip = resources.getString(R.string.server_ip)
+            val retrofit = Retrofit.Builder()
+                .baseUrl(server_ip)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+            val userService = retrofit.create(RetroFitService::class.java)
+            val requestBody = JsonObject()
+            val call = userService.sendgame(requestBody)
+            val r = Runnable {
+                call.enqueue(object : Callback<ResponseBody> {
+                    override fun onResponse(
+                        call: Call<ResponseBody>,
+                        response: Response<ResponseBody>
+                    ) {
+                        val res = response.body()?.string()
+                        try {
+                            val jsonObject = JSONObject(res!!)
+                            val status = jsonObject.getInt("status")
+                            val populargamesimg =jsonObject.getJSONArray("populargames")
+                            val populargames = ArrayList<String>()
+                            for (i in 0 until populargamesimg.length())
+                            {
+                                populargames.add(populargamesimg.getString(i))
+                            }
+                            if (status == 200) {
+
+                                Glide.with(applicationContext)
+                                    .load(populargames[0])
+                                    .centerCrop()
+                                    .into(firstimg)
+                                Glide.with(applicationContext)
+                                    .load(populargames[1])
+                                    .centerCrop()
+                                    .into(secondimg)
+                                Glide.with(applicationContext)
+                                    .load(populargames[2])
+                                    .centerCrop()
+                                    .into(thirdimg)
+                                Glide.with(applicationContext)
+                                    .load(populargames[3])
+                                    .centerCrop()
+                                    .into(fourthimg)
+                                Glide.with(applicationContext)
+                                    .load(populargames[4])
+                                    .centerCrop()
+                                    .into(fiftyimg)
+                                Glide.with(applicationContext)
+                                    .load(populargames[5])
+                                    .centerCrop()
+                                    .into(sixtyimg)
+                            }
+
+                        } catch (e: JSONException) {
+                            // Handle JSON parsing error
+                            // ...
+                        }
+                    }
+
+                    override fun onFailure(calll: Call<ResponseBody>, t: Throwable) {
+                        Toast.makeText(applicationContext, "Error", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                })
+            }
+            val t = Thread(r)
+            t.start()
+        }
         fun searhgame() {
             val nameText = searchgametext.text.toString()
             val server_ip = resources.getString(R.string.server_ip)
@@ -64,9 +140,15 @@ class HomeActivity : AppCompatActivity() {
                         try {
                             val jsonObject = JSONObject(res!!)
                             val status = jsonObject.getInt("status")
-                            val msm = jsonObject.getString("message")
+                            val msm =jsonObject.getJSONArray("game")
+                            val Names = ArrayList<String>()
+                            for (i in 0 until msm.length())
+                            {
+                                Names.add(msm.getString(i))
+                           }
                             if (status == 200) {
-                                recyclerView.adapter = RecViewHomeAdapter(listOf(msm), ContextCompat.getColor(applicationContext, R.color.gold20))
+
+                                recyclerView.adapter = RecViewHomeAdapter(Names  , ContextCompat.getColor(applicationContext, R.color.gold20))
                                 recyclerView.layoutManager = LinearLayoutManager(applicationContext)
                             }
 
@@ -85,7 +167,7 @@ class HomeActivity : AppCompatActivity() {
             val t = Thread(r)
             t.start()
         }
-
+        imgpopular()
         searchgametext.addTextChangedListener(object : TextWatcher{
             override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
 
